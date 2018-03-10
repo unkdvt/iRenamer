747063168f919a3f4d59c1425166fc16c60c75b5
user <user@DESKTOP-PFB8ETA>
2018-03-10 17:14:45 +0530

    init

--- iRenamer/Form1.Designer.cs
+++ iRenamer/Form1.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblpath = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path : ";
+            // 
+            // lblpath
+            // 
+            this.lblpath.AutoEllipsis = true;
+            this.lblpath.AutoSize = true;
+            this.lblpath.Location = new System.Drawing.Point(47, 9);
+            this.lblpath.Name = "lblpath";
+            this.lblpath.Size = new System.Drawing.Size(13, 13);
+            this.lblpath.TabIndex = 2;
+            this.lblpath.Text = "--";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(81, 498);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(99, 59);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(341, 498);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(446, 59);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(450, 498);
+            this.listBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 569);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblpath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblpath;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- iRenamer/Form1.cs
+++ iRenamer/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,37 @@ namespace iRenamer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox3.Items.Clear();
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    lblpath.Text = folderBrowserDialog1.SelectedPath;
+                    DirectoryInfo d = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+
+                    foreach (DirectoryInfo s in d.GetDirectories())
+                    {                                                
+                        string name = s.FullName.Remove(0, s.FullName.LastIndexOf('\\') + 1).Trim();
+                        string[] a = name.Split(new char[] { ' ' }, 2);
+                        if (a.Length > 1)                        {
+                            string number = a[0].Trim();
+                            string fullname = a[1].Trim();
+                            listBox1.Items.Add(number);
+                            listBox2.Items.Add(fullname);
+                            listBox3.Items.Add(fullname + " - " + number);
+                        }
+                    }
+                    System.IO.Directory.CreateDirectory(folderBrowserDialog1.SelectedPath + "\\rename\\" + name.Split(' ')[1] + ' ' + name.Split(' ')[0]);
+                }
+
+            }
+            catch { }
         }
     }
 }
